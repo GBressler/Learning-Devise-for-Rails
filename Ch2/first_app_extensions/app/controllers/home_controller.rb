f6554d3a348c74d8d55d78a7af8611dbe0402d1e
GBressler <gregbressler@gmail.com>
2015-06-09 09:59:35 -0500

    Initial Commit

---
+++ Ch2/first_app_extensions/app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+class HomeController < ApplicationController
+  before_filter :authenticate_user!, :only => [:index, :new]
+  def index
+
+  end
+
+  def show
+  	
+  end
+
+	def new
+		
+	end
+
+	def edit
+		
+	end
+end
